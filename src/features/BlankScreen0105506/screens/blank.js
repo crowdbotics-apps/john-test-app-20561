2c096f0c8205cfbfaae07356e6708a99b62fb20b
Crowdbotics <team@crowdbotics.com>
2020-09-22 15:16:27 +0000

    Update components

--- src/features/BlankScreen0105506/screens/blank.js
+++ src/features/BlankScreen0105506/screens/blank.js
@@ -69,7 +69,7 @@ export class _Blank extends React.Component {
                 resizeMode="cover"
                 source={{
                   uri:
-                    "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/vincent-van-zalinge-44090-unsplash.jpg"
+                    "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/anders-nord-459835-unsplash.jpg"
                 }}
                 style={this.props.themedStyle.Image_17}
               />
@@ -79,7 +79,7 @@ export class _Blank extends React.Component {
                 resizeMode="cover"
                 source={{
                   uri:
-                    "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/scott-johnson-522639-unsplash.jpg"
+                    "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/hugo-kemmel-276408-unsplash.jpg"
                 }}
                 style={this.props.themedStyle.Image_15}
               />
@@ -89,11 +89,69 @@ export class _Blank extends React.Component {
                 resizeMode="cover"
                 source={{
                   uri:
-                    "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/philip-marsh-422399-unsplash.jpg"
+                    "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/hakon-helberg-99985-unsplash_iazWmL4.jpg"
                 }}
                 style={this.props.themedStyle.Image_16}
               />
             </View>
+          </View>
+          <View style={this.props.themedStyle.View_19}>
+            <View style={this.props.themedStyle.View_14}>
+              <Image
+                resizeMode="cover"
+                source={{
+                  uri:
+                    "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/alfred-leung-332452-unsplash.jpg"
+                }}
+                style={this.props.themedStyle.Image_17}
+              />
+            </View>
+            <View style={this.props.themedStyle.View_12}>
+              <Image
+                resizeMode="cover"
+                source={{
+                  uri:
+                    "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/joe-ridley-beth-martin-48011-unsplash.jpg"
+                }}
+                style={this.props.themedStyle.Image_15}
+              />
+            </View>
+            <View style={this.props.themedStyle.View_13}>
+              <Image
+                resizeMode="cover"
+                source={{
+                  uri:
+                    "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/richard-lee-518621-unsplash.jpg"
+                }}
+                style={this.props.themedStyle.Image_16}
+              />
+            </View>
+          </View>
+          <View style={this.props.themedStyle.View_20}>
+            <Image
+              resizeMode="contain"
+              source={{
+                uri:
+                  "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/Vector_4.png"
+              }}
+              style={this.props.themedStyle.Image_23}
+            />
+            <Image
+              resizeMode="contain"
+              source={{
+                uri:
+                  "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/Subtract.png"
+              }}
+              style={this.props.themedStyle.Image_24}
+            />
+            <Image
+              resizeMode="contain"
+              source={{
+                uri:
+                  "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/Vector_5.png"
+              }}
+              style={this.props.themedStyle.Image_25}
+            />
           </View>
         </View>
       </View>
@@ -122,8 +180,8 @@ export default Blank = withStyles(_Blank, theme => ({
     flexDirection: "column",
     flexWrap: "wrap",
     justifyContent: "flex-start",
-    alignItems: "stretch",
-    alignContent: "stretch",
+    alignItems: "flex-start",
+    alignContent: "flex-start",
     borderColor: "#000000",
     borderStyle: "solid",
     borderWidth: 0,
@@ -145,7 +203,7 @@ export default Blank = withStyles(_Blank, theme => ({
     paddingRight: 0,
     paddingTop: 0,
     paddingBottom: 0,
-    overflow: "visible",
+    overflow: "hidden",
     alignSelf: "flex-start",
     fontSize: 12,
     color: "#000000",
@@ -153,10 +211,10 @@ export default Blank = withStyles(_Blank, theme => ({
     fontStyle: "normal",
     fontWeight: "normal",
     flexDirection: "column",
-    flexWrap: "wrap",
+    flexWrap: "nowrap",
     justifyContent: "flex-start",
-    alignItems: "stretch",
-    alignContent: "stretch",
+    alignItems: "flex-start",
+    alignContent: "flex-start",
     borderColor: "#000000",
     borderStyle: "solid",
     borderWidth: 0,
@@ -176,7 +234,7 @@ export default Blank = withStyles(_Blank, theme => ({
     paddingRight: 5,
     paddingTop: 5,
     paddingBottom: 5,
-    overflow: "visible",
+    overflow: "hidden",
     alignSelf: "center",
     fontSize: 17,
     color: "#2c038c",
@@ -231,7 +289,7 @@ export default Blank = withStyles(_Blank, theme => ({
   },
   View_6: {
     width: "100%",
-    height: "100%",
+    height: 100,
     marginLeft: 0,
     marginRight: 0,
     marginTop: 0,
@@ -280,7 +338,7 @@ export default Blank = withStyles(_Blank, theme => ({
     flexDirection: "row",
     flexWrap: "nowrap",
     justifyContent: "space-evenly",
-    alignItems: "flex-start",
+    alignItems: "center",
     alignContent: "flex-start",
     borderColor: "#000000",
     borderStyle: "solid",
@@ -304,7 +362,6 @@ export default Blank = withStyles(_Blank, theme => ({
     paddingTop: 0,
     paddingBottom: 0,
     overflow: "visible",
-    alignSelf: "flex-start",
     fontSize: 12,
     color: "#000000",
     backgroundColor: "#ffffff",
@@ -455,11 +512,11 @@ export default Blank = withStyles(_Blank, theme => ({
   },
   View_18: {
     width: "100%",
-    height: "100%",
+    height: 100,
     marginLeft: 0,
     marginRight: 0,
     marginTop: 0,
-    marginBottom: 0,
+    marginBottom: 11,
     paddingLeft: 0,
     paddingRight: 0,
     paddingTop: 0,
@@ -473,7 +530,7 @@ export default Blank = withStyles(_Blank, theme => ({
     flexDirection: "row",
     flexWrap: "nowrap",
     justifyContent: "space-evenly",
-    alignItems: "flex-start",
+    alignItems: "center",
     alignContent: "flex-start",
     borderColor: "#000000",
     borderStyle: "solid",
@@ -497,7 +554,6 @@ export default Blank = withStyles(_Blank, theme => ({
     paddingTop: 0,
     paddingBottom: 0,
     overflow: "visible",
-    alignSelf: "flex-start",
     fontSize: 12,
     color: "#000000",
     backgroundColor: "#ffffff",
@@ -637,6 +693,297 @@ export default Blank = withStyles(_Blank, theme => ({
     paddingTop: 0,
     paddingBottom: 0,
     overflow: "hidden",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0
+  },
+  View_19: {
+    width: "100%",
+    height: 100,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    alignContent: "flex-start",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    backgroundSize: "auto"
+  },
+  View_14: {
+    width: 60,
+    height: 100,
+    marginLeft: 8,
+    marginRight: 4,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    alignContent: "flex-start",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    backgroundSize: "auto"
+  },
+  Image_17: {
+    width: "100%",
+    height: "100%",
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "hidden",
+    alignSelf: "flex-start",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0
+  },
+  View_12: {
+    width: 60,
+    height: 100,
+    marginLeft: 4,
+    marginRight: 4,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    alignContent: "stretch",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    backgroundSize: "auto"
+  },
+  Image_15: {
+    width: "100%",
+    height: 200,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "hidden",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0
+  },
+  View_13: {
+    width: 60,
+    height: 100,
+    marginLeft: 4,
+    marginRight: 8,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "visible",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    alignContent: "stretch",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    backgroundSize: "auto"
+  },
+  Image_16: {
+    width: "100%",
+    height: 200,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "hidden",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0
+  },
+  View_20: {
+    width: "100%",
+    height: 25,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 15,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "visible",
+    alignSelf: "flex-end",
+    fontSize: 12,
+    color: "#ffffff",
+    backgroundColor: "#2c038c",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+    alignContent: "space-between",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    backgroundSize: "auto"
+  },
+  Image_23: {
+    width: "100%",
+    height: 25,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "hidden",
+    alignSelf: "center",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0
+  },
+  Image_24: {
+    width: "100%",
+    height: 25,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "hidden",
+    alignSelf: "center",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0
+  },
+  Image_25: {
+    width: "100%",
+    height: 25,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "hidden",
+    alignSelf: "center",
     borderColor: "#000000",
     borderStyle: "solid",
     borderWidth: 0,
